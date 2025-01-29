f09af817e803f54d1bd90fdddb73284a535bc450
Aleksandr <kostromin63@mail.ru>
2025-01-29 15:47:56 +0300

    saving intermediate work results (сохранение промежуточных результатов работы)

--- project.py
+++ project.py
@@ -15,7 +15,7 @@ class PriceMachine:
         self.result = ''
         self.name_length = 0
         self.result_columns = ['№', 'Наименование', 'цена', 'вес', 'файл', 'цена за кг']
-        self.new_df = pd.DataFrame(columns=['Наименование', 'цена', 'вес'])
+        self.new_df = pd.DataFrame(columns=['№', 'Наименование', 'цена', 'вес', 'файл', 'цена за кг'])
 
     def load_prices(self, file_path=''):
         '''
@@ -49,21 +49,42 @@ class PriceMachine:
         frames = [self.new_df]
         for head in headers:
             #df = pd.read_csv(head[0], header=None, index_col=head[1:4]) #) # pd.read_csv(data[0], usecols=data[1:4])
-            #df = pd.read_csv(head[0], usecols=head[1:4])
-            #df = pd.DataFrame({'Наименование':[], 'цена':[], 'вес':[]})
-            df = pd.read_csv(head[0], header= 0, index_col=head[1:4])
+            df = pd.read_csv(head[0], header=0, index_col=False, usecols=head[1:4], keep_default_na=False, na_filter=False) #names=['№', 'Наименование', 'цена', 'вес', 'файл', 'цена за кг'],
+            #df = pd.DataFrame(head[0], columns={'Наименование':[head[1]], 'цена':[head[2]], 'вес':[head[3]]})
+            #df = pd.read_csv(head[0], header=None , index_col=head[1:4], keep_default_na=False)
+            # ax = df.axes
+            # b = ax[0].values
+            # b = b[0]
+            # ind1 = b[0]
+            # ind2 = str(b[1])
+            # ind3 = str(b[2])
+            col1 = df['наименование']
+
+            #df = pd.read_csv(head[0], index_col=head[1:4])
+
             #df = pd.concat([df, df1], axis=1, ignore_index=True)
             #df.columns = ['Наименование']
             #df = df.rename(columns={'0': '1', '1': '2', '2': '3'}
             # df = df(usecols=['Наименование', 'цена', 'вес'])
-            df = df.get([], default="default_value")
+            #df = df.get([])
+            #ax = df.axes
+            df['файл'] = head[0]
+            #df['цена за кг'] = df['цена'] / df['вес']
+            # col = df.get([0], default="default_value")
+            # a = df[0]
+            # b = df[1]
+            # c = df[2]
+            # d = df[3]
+
 
             #df_reordered = df[0:3]
-            #df = pd.read_csv(head[0], header=1, data={'Наименование':head[1], 'Цена':head[2], 'Вес':head[3]})
             frames.append(df)
             #print(df)
 
-        self.new_df = pd.concat(frames, axis=1, ignore_index=True)
+        new_df = pd.concat(frames, axis=0, ignore_index=True)
+        # new_df['цена за кг'] = new_df['цена'] / new_df['вес']
+#        new_df['№'] = df.index
+        self.new_df = new_df
         #print(self.new_df )
 
         return self.new_df
@@ -100,17 +121,18 @@ class PriceMachine:
         </head>
         <body>
         '''
-        result = result + '\t'+'\t'+'\t'+'\t' + self.new_df.to_html() + '\n'+'</body>'
+        result = result + '\t' + self.new_df.to_html() + '\n'+'</body>'
         html_file = open(fname, "w+", encoding='utf-8')
         html_file.write(result)
         html_file.close()
-        return result
+        self.result = result
+        # return result
     
     def find_text(self, text):
 
-        filtered_df = self.new_df[self.new_df['Наименование'].str.contains(text)]
+        nomenclatures = self.new_df[self.new_df[''].str.contains(text)]
 
-        print(filtered_df)
+        print(nomenclatures)
 
 
 
@@ -128,4 +150,3 @@ if __name__ == "__main__":
 
     print('the end')
     print(pm.export_to_html())
-
